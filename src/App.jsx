1f12f3e79005b21ce99c47079baffa20c1fda558
salaudeen <olayiwolasalaudeen@gmail.com>
2024-04-27 19:15:56 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./Component/About";
+import Footer from "./Component/Footer";
+import Dashboard from "./Component/Dashboard";
+import Form from "./Component/Form";
+import Navbar from "./Component/Navbar";
+import Body from "./Component/Body";
+import SignIn from "./Component/SignIn/SignIn";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
