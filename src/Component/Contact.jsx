8113b57ec6df21c734c26725d047bf840567f64b
salaudeen <olayiwolasalaudeen@gmail.com>
2024-04-30 17:49:37 +0100

    add FAQ and Contact page to the work

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -1,10 +1,30 @@
-// import React from 'react'
+import { useState, useEffect } from "react"; // Import React, useState, and useEffect from 'react'
+
 
 const Contact = () => {
-  return (
-    <div className='mt-60 text-center text-green-500 mb-10'>
-     welcome to <span className="text-bolder hover:text-green-800">Contact page</span> were we have information about our service</div>
-  )
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000); 
+    }, []);
+  
+    return (
+      <div className="mt-40">
+        {loading ? (
+          <div>
+            <div className="flex items-center justify-center h-screen" disabled>
+              <div className="animate-spin h-40 w-40 border-t-4 border-green-900 rounded-full"></div>
+            </div>
+          </div>
+        ) : (
+            <div className='mt-60 text-center text-green-500 mb-10'>
+            welcome to <span className="text-bolder hover:text-green-800">Contact page</span> were we have information about our service</div>
+        )}
+      </div>
+    );
 }
 
 export default Contact
--- src/Component/Faqs.jsx
+++ src/Component/Faqs.jsx
@@ -1,13 +1,34 @@
 // import React from 'react'
+import { useState, useEffect } from "react";
 
 const Faqs = () => {
-  return (
-    <div className=" mt-60">Faqs 
-    <div className="text-center text-green-600 shadow">
-        welcome to FAQ what are your questions?
-    </div>
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
 
-export default Faqs
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="mt-40">
+      {loading ? (
+        <div>
+          <div className="flex items-center justify-center h-screen" disabled>
+            <div className="animate-spin h-40 w-40 border-t-4 border-green-900 rounded-full"></div>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-60 text-center text-green-500 mb-10">
+          <div className="text-center text-green-600 shadow">
+            welcome to FAQ what are your questions?
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Faqs;
+
+
