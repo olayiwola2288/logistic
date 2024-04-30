3ebbcfd72fa11a52292ac9af08442fc9eb8fe96c
salaudeen <olayiwolasalaudeen@gmail.com>
2024-04-30 16:55:02 +0100

    i add new Navbar after correcting the old one

--- src/Component/Faqs.jsx
+++ src/Component/Faqs.jsx
@@ -2,7 +2,11 @@
 
 const Faqs = () => {
   return (
-    <div className=" mt-60">Faqs</div>
+    <div className=" mt-60">Faqs 
+    <div className="text-center text-green-600 shadow">
+        welcome to FAQ what are your questions?
+    </div>
+    </div>
   )
 }
 
