43ffb241d5bcb0b0150bb735b291c6bf87388db7
salaudeen <olayiwolasalaudeen@gmail.com>
2024-05-15 22:24:00 +0100

    add the FAQs and modifying it

--- src/Component/Faqs.jsx
+++ src/Component/Faqs.jsx
@@ -11,10 +11,10 @@ const FaqItem = ({ question, answer }) => {
   return (
     <div className="px-10 shadow mb-5 w-[85%] mx-auto py-5 rounded">
       <div className="flex justify-between">
-        <p>{question}</p>
-        <RiArrowDropDownLine onClick={handleToggle} />
+        <p className=" text-green-800 font-bold text-xl mb-2">{question}</p>
+        <RiArrowDropDownLine onClick={handleToggle}  className=" font-bold text-green-800"/>
       </div>
-      {show && <p>{answer}</p>}
+      {show && <p className=" text-green-800 text-sm">{answer}</p>}
     </div>
   );
 };
